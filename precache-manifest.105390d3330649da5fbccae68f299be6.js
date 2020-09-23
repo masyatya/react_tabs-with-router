9d9614f5ce24e8aa3464e54d44e868bf0b4022e9
Maxym Chernyavskiy <masyatya@gmail.com>
2020-09-23 14:04:27 +0300

    Updates

---
+++ precache-manifest.105390d3330649da5fbccae68f299be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552f29d6621d53c7f187eb17c481f14e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "852b50803528e834e7d0",
+    "url": "./static/css/main.55c18d52.chunk.css"
+  },
+  {
+    "revision": "eb968bddbae13e8661a6",
+    "url": "./static/js/2.01287768.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.01287768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "852b50803528e834e7d0",
+    "url": "./static/js/main.531ff8a1.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
